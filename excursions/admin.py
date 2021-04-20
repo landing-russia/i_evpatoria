4ea355593ec0bf40b505e658c9b4a612043b1605
landing-russia <landing-russia@yandex.ru>
2021-04-20 17:12:52 +0300

    admin begin 2

--- excursions/admin.py
+++ excursions/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Exscursion
 
 
+class ExscursionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'type', 'created_at', 'updated_at', 'is_published')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'type')
+    # fields = ('title', 'category', 'content', 'photo', 'get_photo', 'is_published', 'views', 'created_at', 'updated_at')
+    save_on_top = True
 
-admin.site.register(Exscursion)
+
+admin.site.register(Exscursion, ExscursionAdmin)
