e4fedebee79c9ab456ce1664f951f66df610cd6c
Aleksey <landing-russia@yandex.ru>
2021-04-21 09:00:20 +0300

    first hosting preview

--- excursions/views.py
+++ excursions/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Exscursion
 
 
 def home(request):
     return render(request, 'excursions/index.html', {'section': 'excursions'})
+
+
+class HomeExcursions(ListView):
+    model = Exscursion
+    template_name = 'excursions/index.html'
+    context_object_name = 'excursions'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['section'] = 'excursions'
+        return context
